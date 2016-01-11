b8ddb0956bde9329d1f289a89f89b4285b0c8731
DieEin <savageshadow@abv.bg>
2016-01-11 16:55:29 +0300

    should almost work

--- Program/main.rb
+++ Program/main.rb
@@ -3,20 +3,23 @@ repo_links = []
 repositories = ['Ruby_Repositories', 'CC_Repositories', 'Java_Repositories']
 text_files = ['ruby_repos.txt', 'cc_repos.txt', 'java_repos.txt']
 cloning_folder = 'test'
-expected_extentions = ['.rb', '.cc', '.java']
+expected_extentions = ['.rb', ['.cc', '.cpp', '.h'], '.java']
 counter = 0.to_i
+
+@hash = Hash.new(0)
+@count = 0.to_i
 
 def clone_and_run(repo_links)
 	Dir.chdir(cloning_folder)
 
 	repo_links.each do |link|
-		`git clone #{link}`
+	  `git clone #{link}`
 
-		repo_name = link.split('/').last
+	  repo_name = link.split('/').last
 
-		Dir.chdir(repo_name)
+	  Dir.chdir(repo_name)
 
-		globbing()
+	  globbing()
 
 	  Dir.chdir('..')
 	end
@@ -27,13 +30,32 @@ def globbing
 		if File.file?(filename)
 			after_dot = filename.split('.').last
 
-			run(after_dot, expected_extentions[counter], filename)
+			#run(after_dot, expected_extentions[counter], filename)
+			if after_dot.include?(expected_extentions[counter])
+				parse_file(filename)
+			end
 		elsif File.directory?(filename)
 			Dir.chdir(filename)
 			globbing()
 			Dir.chdir(..)
 		end
 	end
+end
+
+def parse(string)
+	@count += string.scan(/[[:punct:]]/).count
+	words = string.downcase.scan(/\w+/)
+
+	words.each do |word|
+		@hash[word] += 1
+	end
+end
+
+def parse_file(filename)
+	result = File.open(filename, 'r')
+	result = result.read
+
+	parse(result)
 end
 
 def run(extension, expected_extention, filename)
@@ -59,4 +81,8 @@ while true
 
 	repo_links.clear()
 	counter++
+
+	@hash.each do |key, value|
+		puts "#{key},#{value}"
+	end
 end
