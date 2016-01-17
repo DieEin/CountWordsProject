06e27f2e5de40ddd44a50fd4095d633b371c1de7
DieEin <savageshadow@abv.bg>
2016-01-17 18:33:05 +0300

    little changes

--- Program/lib/clone.rb
+++ Program/lib/clone.rb
@@ -12,12 +12,16 @@ class Clone
 		@repositories_to_csv = []
 		@cloning_folder = 'test'
 		@go_back = '..'
+
+		@change_names = 0.to_i
 	end
 
 	def run
 		Dir.chdir(@cloning_folder)
 
 		@repo_links.each do |link|
+			`mkdir #{@change_names}`
+			Dir.chdir("./#{@change_names}/")
 			`git clone #{link}`
 
 			repo_name = link.split('/').last
@@ -33,6 +37,8 @@ class Clone
 			@repositories_to_csv << "#{link},#{@lines_num}\n"
 
 			Dir.chdir(@go_back)
+			Dir.chdir(@go_back)
+			@change_names += 1
 			@lines_num = 0.to_i
 		end
 		return @repositories_to_csv, @count, @words_num, @hash
